9cf80873b3560066965dde267e8fdb863d913cfb
weinama <vena1028@gmail.com>
2016-03-07 13:37:46 -0500

    Issue #10: build rpm/deb packages

---
+++ src/main/resources/bin/jaguar-daemon.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# source environment
+if [[ -e "${JAGUAR_CONF_DIR}/jaguar-env.sh" ]]; then
+  . "${JAGUAR_CONF_DIR}/jaguar-env.sh"
+else
+  echo "ERROR: Cannot execute ${JAGUAR_CONF_DIR}/jaguar-env.sh." 2>&1
+  exit 1
+fi
+
+# start/stop jaguar server
+echo "Starting jaguar server"
+# exec "${JAGUAR_HOME}/bin/jaguar" "$@"
+java -classpath  ".:${JAGUAR_HOME}/lib/*" com.teraproc.jaguar.JaguarApplication --spring.config.location=${JAGUAR_CONF_DIR}/jaguar.properties --logging.config=${JAGUAR_CONF_DIR}/logback.xml
